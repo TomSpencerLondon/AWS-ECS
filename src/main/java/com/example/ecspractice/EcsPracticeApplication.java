1b4bb3714b0ede62c6c16ef02ae7cc8c0d92fe62
Tom Spencer <thomas.spencer@codurance.com>
2022-11-14 18:48:41 +0000

    add code for starter spring + dockerfile

---
+++ src/main/java/com/example/ecspractice/EcsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EcsPracticeApplication.class, args);
+  }
+
+}
